e21bcc378390dafc46f0ba876088289e991de9c2
Roman Emiliano Arenas <tuemail@ejemplo.com>
2026-04-06 15:42:29 -0300

    fix settings

--- ref_system/settings.py
+++ ref_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver','ref-ya68.onrender.com']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'icia-monitoreo,127.0.0.1,localhost').split(',')
 
 
 # Application definition
